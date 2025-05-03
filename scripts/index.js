eef9f6821d25e8d9ab3bbe1e6bbfe00b2aac9433
SerbianGhost <serbianghost816@gmail.com>
2025-05-03 11:30:14 +0500

    index.js create function for cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,19 @@
-// @todo: Темплейт карточки
+import initialCards from "./cards.js";
+const placecList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
+const createCard = (content, deleteFunc) => {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = content.link;
+  cardElement.querySelector(".card__title").textContent = content.name;
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteFunc);
+  return cardElement;
+};
+const deleteCard = (e) => {
+  e.target.closest(".card").remove();
+};
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach((cardContent) => {
+  const card = createCard(cardContent, deleteCard);
+  placecList.append(card);
+});
